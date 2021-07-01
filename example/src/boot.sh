f0b45aec41b9466b1bd9ef9035ad95715e772769
yw9381 <yw9381@163.com>
2021-07-01 16:02:46 +0800

    change timeout to 600s

--- example/src/boot.sh
+++ example/src/boot.sh
@@ -4,7 +4,7 @@ export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 cd `dirname $0`
 
-exec timeout -s 9 120s qemu-system-x86_64  \
+exec timeout -s 9 300s qemu-system-x86_64  \
     -m 64M \
     -cpu kvm64,+smep,+smap \
     -kernel bzImage \
